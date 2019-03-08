450a0ef598a8869810aa097a47991f5ee4a972dc
Milan Sevarika <sevarikamilan97@gmail.com>
2019-03-08 17:31:33 +0100

    asdf

---
+++ RSOKLab03/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSOKLab03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Milan Sevarika");
+            Console.ReadLine();
+        }
+    }
+}
